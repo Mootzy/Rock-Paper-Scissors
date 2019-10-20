e2d250e763c23b7c5827397ba8d42f65f88b9465
Tyler Wallace <tyler77156@gmail.com>
2019-10-19 22:39:42 -0600

    update css n JS

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let userScore = 0;
-let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const compScore_span = document.getElementById("comp-score");
+let userScore = 0;//set variable for userScore
+let computerScore = 0;//set variable for computerscore
+const userScore_span = document.getElementById("user-score");//finds the ID "user-score" in HTML 
+const compScore_span = document.getElementById("comp-score");//find the ID "comp-score" in HTML
 const scoreboard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");//finds the rock div "r" in HTML
+const paper_div = document.getElementById("p");//finds the paper div "p" in HTML
 const scissors_div = document.getElementById("s");
 
 function getComputerChoice() {
@@ -13,16 +13,34 @@ function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
-function win() {
+function convertToWord(letter) {
+  if (letter === "r") return "Rock ";
+  if (letter === "p") return "Paper ";
+  return "Scissors ";
+}
+
+function win(userChoice, computerChoice) {
   userScore++;
   userScore_span.innerHTML = userScore
-}
-function lose() {
-  computerScore++
   compScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub()
+  const smallCompWord = "comp".fontsize(3).sub()
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beat  ${convertToWord(computerChoice)}${smallCompWord} You win!....`
+  document.getElementById(userChoice).classList.add('green-glow')
 }
-function draw() {
-  console.log("DRAW");
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore
+  compScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub()
+  const smallCompWord = "comp".fontsize(3).sub()
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} lost to  ${convertToWord(computerChoice)}${smallCompWord} You Lost!`
+}
+function draw(userChoice, computerChoice) {
+  
+  const smallUserWord = "user".fontsize(3).sub()
+  const smallCompWord = "comp".fontsize(3).sub()
+  result_p.innerHTML = "It's a draw...."
 }
 console.log(getComputerChoice());
 
@@ -32,17 +50,17 @@ function game(userChoice) {
     case "rs":
     case "pr":
     case "sp":
-      win();
+      win(userChoice, computerChoice);
       break;
     case "rp":
     case "ps":
     case "sr":
-      lose();
+      lose(userChoice, computerChoice);
       break;
     case "rr":
     case "pp":
     case "ss":
-      draw();
+      draw(userChoice, computerChoice);
       break;
   }
 }
